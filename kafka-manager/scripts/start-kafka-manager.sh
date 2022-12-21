9bce936e47c49bf022026a6da390aa333ee87bda
Piyush Kumar <piyush.k9013@gmail.com>
2022-12-21 22:39:12 +0530

    adding more listener examples with dead letter topic recoverer

--- kafka-manager/scripts/start-kafka-manager.sh
+++ kafka-manager/scripts/start-kafka-manager.sh
@@ -1,1 +1,4 @@
 $CMAK_HOME/bin/cmak -Dconfig.file=$CMAK_HOME/conf/application.conf -Dhttp.port=8080
+
+# Uncomment this for loading css properly if above command is not loading css properly
+# $CMAK_HOME/bin/cmak -Dconfig.file=$CMAK_HOME/conf/application.conf -Dhttp.port=8080 -J--add-opens=java.base/sun.net.www.protocol.file=ALL-UNNAMED -J--add-exports=java.base/sun.net.www.protocol.file=ALL-UNNAMED
